f784b9e95725f34e813be20d640a32a46e8145a9
Gabriel Nunes da Silva <ggabrielnunes@gmail.com>
2017-04-24 22:15:45 -0300

    Started working on Minimap

---
+++ Assets/Minimap/MinimapObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SideMiniMap
+{
+    public class MinimapObject : MonoBehaviour
+    {
+        public TYPE objectType;
+        public Transform targetTransform;
+        public MinimapMarker marker;
+        private bool _init = false;
+
+        public void OnEnable()
+        {
+            if(!_init && marker!=null)
+            {
+                marker = Instantiate(marker, targetTransform);
+
+                _init = true;
+            }
+        }
+    }
+}
