d8b3ed4159b5e94ad267618b9ad79a41581addcf
GordoJoe <leonardo2845@hotmail.com>
2017-05-11 10:38:08 -0300

    spawn de minion do amarillo
    ele mexeu

--- Assets/Script/Atirar.cs
+++ Assets/Script/Atirar.cs
@@ -6,23 +6,37 @@ public class Atirar : NetworkBehaviour {
 
     public float tempo = 10;
     public GameObject MinionBK;
+    public GameObject MinionBKTop;
     public GameObject MinionTE;
-
+    public GameObject MinionTETop;
     private void vai()
     {
         tempo = 10.0f;
         
             MinionBK = Resources.Load("MinionBK") as GameObject;
             MinionTE = Resources.Load("MinionTE") as GameObject;
-            
+        MinionBKTop = Resources.Load("MinionBKTop") as GameObject;
+        MinionTETop = Resources.Load("MinionTETop") as GameObject;
+
         var Gera = Instantiate(MinionBK);
         Gera.transform.position = GameObject.FindGameObjectWithTag("BaseBatata").transform.position;
 
         Gera = Instantiate(MinionTE);
         Gera.transform.position = GameObject.FindGameObjectWithTag("BaseTomate").transform.position;
 
+        Gera = Instantiate(MinionBKTop);
+        Gera.transform.position = GameObject.FindGameObjectWithTag("BaseTomate").transform.position;
+
+        Gera = Instantiate(MinionTETop);
+        Gera.transform.position = GameObject.FindGameObjectWithTag("BaseTomate").transform.position;
+
+
+
         NetworkServer.Spawn(MinionBK);
         NetworkServer.Spawn(MinionTE);
+
+        NetworkServer.Spawn(MinionBKTop);
+        NetworkServer.Spawn(MinionTETop);
     }
 
    void Update()
